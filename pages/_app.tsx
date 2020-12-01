f578f05851bcbb4440853b5da115e056d15e461f
Kjintae <wlsxo2112@naver.com>
2020-12-01 22:41:40 +0900

    [Add] antd-design setting

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,15 @@ import { AppProps } from "next/app";
 import { ThemeProvider } from "../styles/styled-components";
 import { GlobalStyle } from "../styles/global-styles";
 import { theme } from "../styles/theme";
+import "antd/dist/antd.css";
 
 function App({ Component, pageProps }: AppProps) {
-  return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Component {...pageProps} />
-    </ThemeProvider>
-  );
+	return (
+		<ThemeProvider theme={theme}>
+			<GlobalStyle />
+			<Component {...pageProps} />
+		</ThemeProvider>
+	);
 }
 
 export default App;
