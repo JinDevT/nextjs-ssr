f35c507ac5cac79e711264022ba2f57e7178c80d
Kjintae <wlsxo2112@naver.com>
2020-12-01 23:21:37 +0900

    [Add] 반응형 (Grid) 적용

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Link from "next/link";
-import { Menu } from "antd";
+import { Menu, Input, Row, Col } from "antd";
 
 interface Props {
 	children: React.ReactNode;
 }
 
 // 일부애들이 공통인 애들
+// 반응형: 가로 -> 세로, 모바일 -> 태블릿 -> 데스크탑 순서로 하는게 편함.
 function Layout({ children }: Props) {
 	return (
 		<div>
@@ -22,12 +23,30 @@ function Layout({ children }: Props) {
 					</Link>
 				</Menu.Item>
 				<Menu.Item>
+					<Input.Search style={{ verticalAlign: "middle" }} />
+				</Menu.Item>
+				<Menu.Item>
 					<Link href="/signup">
 						<a>회원가입</a>
 					</Link>
 				</Menu.Item>
 			</Menu>
-			{children}
+			{/* gutter: 컬럼사이의 간격 */}
+			<Row gutter={8}>
+				{/* 24등분 한다고 생각하면 됨. 24로 나눈이유: 등분하기 좋은 숫자. */}
+				<Col xs={24} md={6}>
+					왼쪽메뉴
+				</Col>
+				<Col xs={24} md={12}>
+					{children}
+				</Col>
+				<Col xs={24} md={6}>
+					{/* target="_blank" 를 쓸 때 보안을 위해 rel="noreferrer noopener" 꼭 적어준다*/}
+					<a href="https://jindev-t.tistory.com/" target="_blank" rel="noreferrer noopener">
+						블로그
+					</a>
+				</Col>
+			</Row>
 		</div>
 	);
 }
