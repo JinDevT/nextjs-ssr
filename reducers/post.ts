883872a3263e92112444af2fccbdcd102f7eb42c
Kjintae <wlsxo2112@naver.com>
2020-12-14 22:33:57 +0900

    [Add] post reducer 구 - initialState -> dummydata 적용

--- reducers/post.ts
+++ reducers/post.ts
@@ -1,9 +1,56 @@
 export const initialState = {
-  mainPosts: [],
+  mainPosts: [
+    {
+      id: 1,
+      User: {
+        id: 1,
+        nickname: "JinDevT",
+      },
+      content: "첫 번째 게시글 #해시태그",
+      Comments: [
+        {
+          User: {
+            nickname: "dev",
+          },
+          content: "우와",
+        },
+        {
+          User: {
+            nickname: "max",
+          },
+          content: "프론트 개발자",
+        },
+      ],
+    },
+  ],
+  imagePaths: [],
+  postAdded: false,
+};
+
+const ADD_POST = "POST/ADD_POST";
+export const addPost = {
+  type: ADD_POST,
+};
+
+const dummyPost = {
+  id: 2,
+  content: "더미데이터",
+  User: {
+    id: 1,
+    nickname: "JinDevT",
+  },
+  Images: [],
+  Comments: [],
 };
 
 const postReducer = (state = initialState, action) => {
   switch (action.type) {
+    case ADD_POST:
+      return {
+        ...state,
+        mainPosts: [dummyPost, ...state.mainPosts],
+        postAdded: true,
+      };
     default:
       return state;
   }
