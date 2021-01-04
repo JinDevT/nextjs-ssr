60f3986475c2204c969ed6881695e535df6930e0
Kjintae <wlsxo2112@naver.com>
2021-01-04 21:44:39 +0900

    [Add] liked, comment 구현 - toggle 구현 시 유용한 코드 setState(prev => !prev)

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 import { PostsState } from "../@type/post";
 import { Card, Popover, Button } from "antd";
 import {
@@ -6,6 +6,7 @@ import {
   HeartOutlined,
   MessageOutlined,
   EllipsisOutlined,
+  HeartTwoTone,
 } from "@ant-design/icons";
 import ButtonGroup from "antd/lib/button/button-group";
 import { useSelector } from "react-redux";
@@ -17,16 +18,36 @@ interface Props {
 }
 function PostCard({ post }: Props) {
   console.log("postssssss: ", post);
+  const [liked, setLiked] = useState(false);
+  const [commentFormOpened, setCommentFormOpened] = useState(false);
   const id = useSelector((state: RootState) => state.user.me?.id);
   // const id = me?.id -> 옵셔널 체이닝 문법: 없으면 undefined를 명시
+
+  const onToggleLike = useCallback(() => {
+    setLiked(prev => !prev); // true false를 조정할 떄 이런식으로!
+    // 이전 데이트 기반으로 다음 데이터를 만든다.
+  }, []);
+
+  const onToggleComment = useCallback(() => {
+    setCommentFormOpened(prev => !prev);
+  }, []);
   return (
     <div style={{ marginBottom: 20 }}>
       <Card
         cover={post.Images[0] && <PostImages images={post.Images} />}
         actions={[
           <RetweetOutlined key="retweet" />,
-          <HeartOutlined key="heart" />,
-          <MessageOutlined key="comment" />,
+          // {
+          //   liked ?
+          //   <HeartTwoTone key="heart" />
+          //   :  <HeartOutlined key="heart" />,
+          liked ? (
+            <HeartTwoTone twoToneColor="#eb2f96" key="heart" onClick={onToggleLike} />
+          ) : (
+            <HeartOutlined key="heart" onClick={onToggleLike} />
+          ),
+
+          <MessageOutlined key="comment" onClick={onToggleComment} />,
           <Popover
             key="more"
             content={[
@@ -52,6 +73,7 @@ function PostCard({ post }: Props) {
           description={post.content}
         />
       </Card>
+      {commentFormOpened && <div>댓글부분</div>}
     </div>
   );
 }
