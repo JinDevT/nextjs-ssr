7eb39a09d023d7bc3d8d4c03c2df7e9e399292d7
Kjintae <wlsxo2112@naver.com>
2021-01-13 22:40:17 +0900

    [Fix] user.ts reducer saga에 맞게 수정중

--- reducers/user.ts
+++ reducers/user.ts
@@ -7,6 +7,7 @@ const dummyUser = {
 };
 
 export const initialState = {
+  isLoggingIn: false, // 로그인 시도중
   isLoggedIn: false,
   me: null,
   signUpData: {},
@@ -15,10 +16,12 @@ export const initialState = {
 
 export const SIGN_UP = "SIGN_UP";
 export const SIGN_UP_SUCCESS = "SIGN_UP_SUCCESS";
-export const LOG_IN = "LOG_IN"; // 액션의 이름
+export const LOG_IN_REQUEST = "LOG_IN_REQUEST"; // 액션의 이름
 export const LOG_IN_SUCCESS = "LOG_IN_SUCCESS"; // 액션의 이름
 export const LOG_IN_FAILURE = "LOG_IN_FAILURE"; // 액션의 이름
-export const LOG_OUT = "LOG_OUT";
+export const LOG_OUT_REQUEST = "LOG_OUT_REQUEST";
+export const LOG_OUT_SUCCESS = "LOG_OUT_SUCCESS";
+export const LOG_OUT_FAILURE = "LOG_OUT_FAILURE";
 
 export const signUpAction = data => {
   return {
@@ -31,15 +34,15 @@ export const signUpSuccess = {
   type: SIGN_UP_SUCCESS,
 };
 
-export const loginAction = data => {
+export const loginRequestAction = data => {
   return {
-    type: LOG_IN,
+    type: LOG_IN_REQUEST,
     data,
   };
 };
-export const logoutAction = () => {
+export const logoutRequestAction = () => {
   return {
-    type: LOG_OUT,
+    type: LOG_OUT_REQUEST,
   };
 };
 export const signUp = data => {
@@ -51,15 +54,42 @@ export const signUp = data => {
 
 const useReducer = (state = initialState, action) => {
   switch (action.type) {
-    case LOG_IN: {
+    case LOG_IN_REQUEST: {
       return {
         ...state,
+        isLoggingIn: true,
+      };
+    }
+    case LOG_IN_SUCCESS: {
+      return {
+        ...state,
+        isLoggingIn: false,
         isLoggedIn: true,
-        user: dummyUser,
         loginData: action.data,
       };
     }
-    case LOG_OUT: {
+    case LOG_IN_FAILURE: {
+      return {
+        ...state,
+        isLoggingIn: false,
+        isLoggedIn: true,
+      };
+    }
+    case LOG_OUT_REQUEST: {
+      return {
+        ...state,
+        isLoggedIn: false,
+        user: null,
+      };
+    }
+    case LOG_OUT_SUCCESS: {
+      return {
+        ...state,
+        isLoggedIn: false,
+        user: null,
+      };
+    }
+    case LOG_OUT_FAILURE: {
       return {
         ...state,
         isLoggedIn: false,
