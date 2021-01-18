7b75a98aaa147d6b5b0593715ceeceb89532f07a
Kjintae <wlsxo2112@naver.com>
2021-01-18 22:30:11 +0900

    [ADD] signup, follow, unfollow action 추가 [Fix] action에 맞게 reducer 조정 및 initailState 수정

--- reducers/user.ts
+++ reducers/user.ts
@@ -1,32 +1,50 @@
-const dummyUser = {
+const dummyUser = data => ({
+  ...data,
   id: 1,
   nickname: "JinDevT",
   Posts: [],
   Followings: [],
   Followers: [],
-};
+});
 
 export const initialState = {
-  isLoggingIn: false, // 로그인 시도중
-  isLoggedIn: false,
-  isLoggingOut: false, // 로그아웃 시도중
+  logInLoading: false, // 로그인 시도중
+  logInDone: false,
+  logInError: null,
+  logOutLoading: false, // 로그아웃 시도중
+  logOutDone: false,
+  logOutError: null,
+  signUpLoading: false, // 회원가입 시도중
+  signUpDone: false,
+  signUpError: null,
   me: null,
   signUpData: {},
   loginData: {},
 };
 
-export const SIGN_UP = "SIGN_UP";
+export const SIGN_UP_REQUEST = "SIGN_UP_REQUEST";
 export const SIGN_UP_SUCCESS = "SIGN_UP_SUCCESS";
+export const SIGN_UP_FAILURE = "SIGN_UP_FAILURE";
+
 export const LOG_IN_REQUEST = "LOG_IN_REQUEST"; // 액션의 이름
 export const LOG_IN_SUCCESS = "LOG_IN_SUCCESS"; // 액션의 이름
 export const LOG_IN_FAILURE = "LOG_IN_FAILURE"; // 액션의 이름
+
 export const LOG_OUT_REQUEST = "LOG_OUT_REQUEST";
 export const LOG_OUT_SUCCESS = "LOG_OUT_SUCCESS";
 export const LOG_OUT_FAILURE = "LOG_OUT_FAILURE";
 
+export const FOLLOW_REQUEST = "FOLLOW_REQUEST";
+export const FOLLOW_SUCCESS = "FOLLOW_SUCCESS";
+export const FOLLOW_FAILURE = "FOLLOW_FAILURE";
+
+export const UNFOLLOW_REQUEST = "UNFOLLOW_REQUEST";
+export const UNFOLLOW_SUCCESS = "UNFOLLOW_SUCCESS";
+export const UNFOLLOW_FAILURE = "UNFOLLOW_FAILURE";
+
 export const signUpAction = data => {
   return {
-    type: SIGN_UP,
+    type: SIGN_UP_REQUEST,
     data,
   };
 };
@@ -46,58 +64,57 @@ export const logoutRequestAction = () => {
     type: LOG_OUT_REQUEST,
   };
 };
-export const signUp = data => {
-  return {
-    type: SIGN_UP,
-    data,
-  };
-};
 
 const useReducer = (state = initialState, action) => {
   switch (action.type) {
     case LOG_IN_REQUEST: {
       return {
         ...state,
-        isLoggingIn: true,
+        logInLoading: true,
+        logInError: null,
+        logInDone: false,
       };
     }
     case LOG_IN_SUCCESS: {
       return {
         ...state,
-        isLoggingIn: false,
-        isLoggedIn: true,
+        logInLoading: false,
+        logInDone: true,
         loginData: action.data,
-        me: { ...action.data, nickname: "Max" },
+        me: dummyUser(action.data),
       };
     }
     case LOG_IN_FAILURE: {
       return {
         ...state,
-        isLoggingIn: false,
-        isLoggedIn: true,
+        logInLoading: false,
+        logInError: action.error,
       };
     }
     case LOG_OUT_REQUEST: {
       return {
         ...state,
-        isLoggingOut: true,
+        logOutLoading: true,
+        logOutDone: false,
+        logOutError: null,
       };
     }
     case LOG_OUT_SUCCESS: {
       return {
         ...state,
-        isLoggingOut: false,
-        isLoggedIn: false,
+        logOutLoading: false,
+        logOutDone: true,
         me: null,
       };
     }
     case LOG_OUT_FAILURE: {
       return {
         ...state,
-        isLoggingOut: false,
+        logOutLoading: false,
+        logOutError: action.error,
       };
     }
-    case SIGN_UP: {
+    case SIGN_UP_REQUEST: {
       return {
         ...state,
         signUpData: action.data,
